f4c1393cfc207968f746e3d285d79df858bf29b8
razzubair <reza.a.wijonarko@gmail.com>
2021-06-27 14:44:33 +0700

    Modified halaman utama view

--- resources/views/layout/v_navbar.blade.php
+++ resources/views/layout/v_navbar.blade.php
@@ -8,7 +8,7 @@
         <a href="{{ route('home') }}" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="{{ route('landing') }}#team" class="nav-link">Kontak</a>
       </li>
     </ul>
 
